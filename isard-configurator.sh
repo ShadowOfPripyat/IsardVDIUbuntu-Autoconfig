dda69d26dd1525e7062d296ed204b7b8c90e3e5a
Vasily Kalashinov <73404930+ShadowOfPripyat@users.noreply.github.com>
2024-01-18 19:28:07 +0100

    Update isard-configurator.sh

--- isard-configurator.sh
+++ isard-configurator.sh
@@ -1,25 +1,52 @@
 #!/bin/bash
 
 # Borra el terminal.
+echo "${RA}"
 clear
-
 # SCRIPT DISSENYAT PER CONFIGURAR MÀQUINES UBUNTU SERVER 22.04 del ISARDVDI
 
-
-# Colors
-R='\e[31m' #Vermell
-G='\e[32m' #Verd
-Y='\e[33m' #Groc
-NC='\e[97' # No Color
+ # Colors
+R='\e[91m' #Vermell
+G='\e[92m' #Verd
+Y='\e[93m' #Groc
+NC='\e[39m' # Color per defecte (blanc)
 RA='\e[0m' #Fer un reset a tots els atributs de format al terminal
-# per cridar als colors, s'han de posar davant del text: echo -e "${COLOR} text". Disponibles ${R} ${G} ${Y} ${NC}
+ # Formatació
+B='\e[1m' #Lletra Negreta
+S='\e[4m' #Lletra subrallada
 
-echo "${G}"
-# Definir la ruta de del fitxer netplan
+# per cridar als colors o formats, s'han de posar davant del text: echo -e "${COLOR} text". Disponibles ${R} ${G} ${Y} ${NC} ${B} ${S}
+
+# Definir la ruta de del fitxer netplan i Definir la ruta de del fitxer PAM (per canviar els requisits de llargada de la contrasenya)
 FITXER_NETPLAN="/etc/netplan/00-installer-config.yaml"
-
-# Definir la ruta de del fitxer PAM (per canviar els requisits de llargada de la contrasenya)
 FITXER_PAM="/etc/pam.d/common-password"
+
+# Genera un número aleatori entre 1 i 65 per la IP.
+randip=$((1 + RANDOM % 65))
+
+echo
+echo
+echo "${B} ${S} Aquest Script configura el netplan, et deixa posar una contrassenya de 1 caracter i t'obre un asistent per canviar la mida de la lletra"
+echo 
+echo
+echo ${RA}
+
+# Funció per introduir una IP personalitzada
+read_custom_ip() {
+  read -p "Enter custom IP address for enp2s0 (press Enter for default 172.16.$randip.10/24): " custom_ip
+}
+
+# Ask user for custom IP
+read -p "vols posar una IP per la targeta "Personal" (enp2s0)? (s/n): " set_custom_ip
+
+case $set_custom_ip in
+  [sS])
+    read_custom_ip
+    ;;
+  *)
+    custom_ip="172.16.$randip.10"
+    ;;
+esac
 
 # Crear o sobreescriure el fitxer de netplan
 cat <<EOL > "$FITXER_NETPLAN"
@@ -29,7 +56,7 @@ network:
       dhcp4: true
     enp2s0:
       addresses:
-        - 172.16.39.10/24
+        - $custom_ip/24
 EOL
 
 # Aplica la configuració del fitxer netplan
@@ -117,5 +144,6 @@ case "$FontChoice" in
     ;;
 esac
 
+echo "${RA}"
 
 # Enllaç amb informació sobre el format de lletra i colors al BASH: https://misc.flogisoft.com/bash/tip_colors_and_formatting
